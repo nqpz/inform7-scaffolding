03adf1606449f3b8991ec73dba51fd5f5288fb9f
Niels G. W. Serup <ngws@metanohi.name>
2025-04-22 22:35:19 +0200

    Add derivations for inweb, intest, and inform
    This will give us the newest versions.

--- default.nix
+++ default.nix
@@ -1,5 +1,7 @@
 pkgs:
 let
+  sources = import ./nix/sources.nix;
+
   scripts = {
     inform7-init = pkgs.writeScriptBin "inform7-init" (''
 #!/bin/sh
@@ -103,6 +105,111 @@ exec ${pkgs.rlwrap}/bin/rlwrap ${pkgs.inform7}/libexec/dumb-glulxe "$@"
   };
 
   programs = {
+    inform7 = let
+      buildInputs = [ pkgs.clang ];
+
+      inweb-dev = pkgs.stdenv.mkDerivation {
+        pname = "inweb";
+        version = "7.2.1-beta+1B67";
+
+        unpackPhase = "true";
+
+        inherit buildInputs;
+
+        configurePhase = ''
+        cp -r ${sources.inweb} inweb
+        chmod u+w -R inweb
+        '';
+
+        buildPhase = ''
+        bash inweb/scripts/first.sh linux
+        '';
+
+        installPhase = ''
+        mkdir $out
+        cp -r inweb $out
+        '';
+      };
+
+      inweb-and-intest-dev = pkgs.stdenv.mkDerivation {
+        pname = "intest";
+        version = "2.2.0-beta+1A60";
+
+        unpackPhase = "true";
+
+        inherit buildInputs;
+
+        configurePhase = ''
+        cp -r ${inweb-dev}/* .
+        cp -r ${sources.intest} intest
+        chmod u+w -R intest
+        '';
+
+        buildPhase = ''
+        bash intest/scripts/first.sh
+        '';
+
+        installPhase = ''
+        mkdir $out
+        cp -r inweb intest $out
+        '';
+      };
+
+      inform7-dev = pkgs.stdenv.mkDerivation {
+        pname = "inform7-dev";
+        version = "10.2.0-beta+6X83";
+
+        unpackPhase = "true";
+
+        inherit buildInputs;
+
+        configurePhase = ''
+        cp -r ${inweb-and-intest-dev}/* .
+        cp -r ${sources.inform} inform
+        chmod u+w -R inform
+        '';
+
+        buildPhase = ''
+        cd inform
+        bash scripts/first.sh
+        '';
+
+        installPhase = ''
+        cp -r . $out
+        '';
+      };
+    in pkgs.stdenv.mkDerivation {
+      pname = "inform7";
+      version = "10.2.0-beta+6X83";
+
+      unpackPhase = "true";
+
+      buildInputs = [];
+
+      configurePhase = "true";
+
+      buildPhase = "true";
+
+      installPhase = ''
+      cp -r ${inform7-dev} $out
+      chmod u+w $out
+      chmod u+w -R $out/inform7
+      rm -r $out/inform7/Internal
+      ln -s /tmp/inform7/Internal $out/inform7/Internal
+      ln -s /tmp/inform7/gameinfo.dbg $out/gameinfo.dbg
+      mkdir $out/bin
+      cat > $out/bin/inbuild <<EOF
+      #!/bin/sh
+      set -e
+      test -d /tmp/inform7/Internal || (mkdir -p /tmp/inform7 && cp -r ${inform7-dev}/inform7/Internal /tmp/inform7/ && chmod u+w /tmp/inform7)
+      test -f /tmp/inform7/gameinfo.dbg || touch /tmp/inform7/gameinfo.dbg
+      cd $out
+      EOF
+      echo 'exec ./inbuild/Tangled/inbuild "$@"' >> $out/bin/inbuild
+      chmod +x $out/bin/inbuild
+      '';
+    };
+
     glulxe = let
       buildInputs = [ pkgs.ncurses.dev ];
 
@@ -185,6 +292,7 @@ clean:
     scripts.inform7-compile
     scripts.inform7-run-basic
     pkgs.util-linux
+    programs.inform7
     programs.glulxe
   ];
 
